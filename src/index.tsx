f407530c60e4f9ada4bc0ed98f5d4d8f00728d78
高云蛟 <869345843@qq.com>
2018-11-22 16:27:02 +0800

    添加react-router进行路由管理

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Router, Route } from 'react-router';
+import { Link } from 'react-router-dom';
 
 
 import { MainPage } from './pages/Main';
