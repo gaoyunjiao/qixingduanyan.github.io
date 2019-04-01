a13f3b41f9efe3a7bdc06b94edcf95e1f766b650
dexiandushu <869345843@qq.com>
2019-04-01 10:53:02 +0800

    添加-lock.json到ignore

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Header, Footer, Container } from "../components";
 import '../themes/themes.less';
+import '../articals/Program-Feature.md'
 
 export interface MainPageProps {}
 
